b754c9ba1b2a57ebb916f9a6e3b5976e530f3e0c
ItsMavey <contact@itsmavey.com>
2026-02-02 11:40:29 -0500

    base commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://adam-ousmer.dev',
+    prefetch:  true,
+
+    compressHTML: true,
+
+    integrations: [sitemap()],
+
+    server: {
+        port: 4321,
+        host: true
+    },
+
+
+    build: {
+        inlineStylesheets: 'auto',
+    }
+});
